dcd8412000b49c747651c4a58f1b029419974d4f
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-01-04 19:02:34 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n *= -1;
+  }
+  let result = [];
+  let string = String(n).split('');
+  for(i = string.length - 1; i >= 0; i--) {
+    if (string[i] !== 0) {
+      result.push(string[i]);
+    }
+  }
+  return Number(result.join(''));
 }
